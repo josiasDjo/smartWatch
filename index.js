228b2c6ab74eb4ed3098ad7372ddc23846bc2045
Besodia kabambi <138811540+djodv@users.noreply.github.com>
2024-04-19 18:45:02 +0200

    commit

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import Draggable from "gsap/Draggable";
-import Flip from "gsap/Flip";
 
-// Sélection de l'élément à animer
-const box = document.querySelector('.box');
 
-// Création de l'animation avec GSAP
-gsap.fromTo(box, {
-    opacity: 0, 
-    y: 100 // Décalage initial vers le bas
-}, {
-    opacity: 1, 
-    y: 0, // Pas de décalage
-    duration: 1, // Durée de l'animation en secondes
-    scrollTrigger: {
-        trigger: box, // Déclencheur : l'élément lui-même
-        start: 'top 80%', // L'animation commence lorsque l'élément est à 80% de la hauteur de la fenêtre
-        end: 'bottom 20%', // L'animation se termine lorsque l'élément est à 20% de la hauteur de la fenêtre
-        scrub: true // L'animation suit le défilement
-    }
-});
+function precedentImg() {
+    console.log("Arrière");
+}
+
+function suivantImg() {
+    console.log("Suivant");
+}
--- pages/explore.php
+++ pages/explore.php
@@ -3,35 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <scrip src="../index.js"></scrip>
     <?php
         require ("pages/pageIconStyle.php");
         echo $styleLink;
         echo $iconLink;
+        // echo $scriptLink;
     ?>
     <title>Smart Watch Shop</title>
 </head>
 <body>
     <div class="container_Explore">
+        <button type="submit" class="btn-left" onclick="precedentImg"><i class="fa-solid fa-chevron-left"></i></button>
         <ul class="gallery-image">
-            <button type="submit" class="btn-left"><i class="fa-solid fa-chevron-left"></i></button>
-            <li clas="block">
+            <li>
                 <img src="./src/images/smart watch/bg remove/watcher (1).png" alt="" class="">
             </li>
-            <li class="hidden">
+            <li>
                 <img src="./src/images/smart watch/bg remove/watcher (2).png" alt="" class="">
             </li>
             <li>
                 <img src="./src/images/smart watch/bg remove/watcher (3).png" alt="" class="">
             </li>
-            <li class="hidden">
+            <li>
                 <img src="./src/images/smart watch/bg remove/watcher (4).png" alt="" class="">
             </li>
-            <li class="hidden">
+            <li>
                 <img src="./src/images/smart watch/bg remove/watcher (5).png" alt="" class="">
             </li>
-            <button type="submit" class="btn-right"><i class="fa-solid fa-chevron-right"></i></i></button>
-            
         </ul>
+        <button type="submit" class="btn-right" onclick="suivantImg"><i class="fa-solid fa-chevron-right"></i></i></button>
+            
     </div>
 </body>
 </html>
